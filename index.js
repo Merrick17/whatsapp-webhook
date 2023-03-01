479615b0fd0337d4960db75638747730958f92b7
safwen ben fredj <safwen.benfredj@gmail.com>
2023-03-01 22:28:35 +0100

    pushing

--- index.js
+++ index.js
@@ -6,10 +6,38 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.post("/webhook", (req, res) => {
-  console.log(req.body);
-  res.status(200).send("OK");
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
+const VERIFY_TOKEN ="test-token-whatsapp"
+app.get("/webhook", (req, res) => {
+  const mode = req.query["hub.mode"];
+  const token = req.query["hub.verify_token"];
+  const challenge = req.query["hub.challenge"];
+
+  if (mode && token === VERIFY_TOKEN) {
+    res.status(200).send(challenge);
+  } else {
+    res.sendStatus(403);
+  }
+});
+
+app.post("/webhook", (req, res) => {
+  const body = req.body;
+
+  // Check if the request is coming from Facebook
+  if (body.object === "page") {
+    body.entry.forEach((entry) => {
+      const webhookEvent = entry.messaging[0];
+      console.log(webhookEvent);
+    });
+
+    res.sendStatus(200);
+  } else {
+    res.sendStatus(404);
+  }
+});
+
 app.listen(port, () => {
-  console.log(`Server is listening on port ${port}`);
+  console.log(`App listening on port ${port}`);
 });
